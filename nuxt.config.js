8d2ebc76bc8cced2ce41f459728e0cd51a34380c
Swen van Zanten <info@hdsign.nl>
2021-04-07 12:21:47 +0200

    add some favicon logic

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,19 +42,7 @@ export default {
       { name: 'twitter:image', content: '/social-banner.png' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '192x192',
-        href: '/android-chrome-192x192.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '512x512',
-        href: '/android-chrome-512x512.png',
-      },
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'icon',
         type: 'image/png',
@@ -72,6 +60,10 @@ export default {
         type: 'image/png',
         sizes: '180x180',
         href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/manifest.json',
       },
     ],
   },
