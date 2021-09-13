d093515abcdfe1e7b2fccf6eecabf42231f3066c
SwenVanZanten <SwenVanZanten@users.noreply.github.com>
2021-09-13 15:28:36 +0000

    deploy: dd65413cbb3f3e51399776ab01181555c8867759

---
+++ _nuxt/static/1631546915/invalid-transaction-request/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631546915",layout:"default",error:a,serverRendered:true,routePath:"\u002Finvalid-transaction-request",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
